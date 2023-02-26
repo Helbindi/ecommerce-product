1059155b9cc3e669a08200399adef61fab7ffc6e
Andy <nunuzuhh@gmail.com>
2023-02-26 09:52:24 -0800

    created shopping cart window. Need to work on data logic for UI items.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
-import cart from "../assets/images/icon-cart.svg";
-import avatar from "../assets/images/image-avatar.png";
+import cartIcon from "../assets/images/icon-cart.svg";
+import avatarIcon from "../assets/images/image-avatar.png";
+import deleteIcon from "../assets/images/icon-delete.svg";
 
 function User() {
+  const [isActive, setIsActive] = useState(false);
+
+  const userCart = (
+    <div className="user-cart">
+      <p className="cart-title">Cart</p>
+      <hr />
+      <div className="cart-product">
+        <img className="item-img" src={cartIcon} alt="" />
+        <p className="item-name">Product Name</p>
+        <p className="item-price">
+          Product Cost x 3 <strong>Total</strong>
+        </p>
+        <img className="delete-img" src={deleteIcon} alt="" />
+        <button className="btn checkout-btn">Checkout</button>
+      </div>
+    </div>
+  );
+
+  function handleActive(e) {
+    e.preventDefault();
+    if (isActive) {
+      setIsActive(false);
+    } else {
+      setIsActive(true);
+    }
+  }
   return (
     <div className="user-container">
-      <img className="cart-img" src={cart} alt="shopping-cart-image" />
-      <img className="avatar-img" src={avatar} alt="user-avatar-image" />
+      <img
+        className="cart-img"
+        src={cartIcon}
+        alt="shopping-cart-image"
+        onClick={(e) => handleActive(e)}
+      />
+      <img
+        className="avatar-img hover"
+        src={avatarIcon}
+        alt="user-avatar-image"
+        onClick={(e) => handleActive(e)}
+      />
+      {isActive && userCart}
     </div>
   );
 }
